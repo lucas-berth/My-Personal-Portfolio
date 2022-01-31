8ff61cc11e749717d66d8e42a185986fe4e2ba06
Lucas Berth <lucas.berth529@gmail.com>
2022-01-31 16:18:25 -0600

    first commit

---
+++ RollingDice.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class RollingDice
+{
+    public static void main(String[] args)
+    {
+        Random rand = new Random();
+        int a = rand.nextInt(6) + 1;
+
+        System.out.println("You rolled a:" + a );
+    }
+}
